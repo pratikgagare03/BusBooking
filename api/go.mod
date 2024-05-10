07571de08147fbfff043185a900429cbd0af0995
Pratik Gagare <pratikgagare302002@gmail.com>
2024-05-10 18:12:11 +0530

    logger, .env

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,12 @@
 module busbooking
 
 go 1.22.2
+
+require github.com/rs/zerolog v1.32.0
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -3,20 +3,40 @@ package main
 import (
 	"busbooking/availability"
 	"busbooking/book"
+	"busbooking/logger"
+	"busbooking/types"
 	"busbooking/utils"
+	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func setupRoutes(buses *[]types.Bus) {
+	http.HandleFunc("/check_availability", availability.Check_availability(buses))
+	http.HandleFunc("/book", book.BookingHandler(buses))
+}
+
+func startServer() {
+	fmt.Printf("localhost started at port%v", os.Getenv("APP_PORT"))
+	err := http.ListenAndServe(os.Getenv("APP_PORT"), nil)
+	if err != nil {
+		logger.Logs.Error().Err(err)
+		return
+	}
+}
+
 func main() {
+	logger.Logs.Info().Msg("Started Main")
+	err := godotenv.Load(".env")
+	if err != nil {
+		logger.Logs.Error().Err(err)
+	}
+	defer logger.File.Close()
 	buses := utils.CreateBuses()
-	// Register handlers
+	setupRoutes(buses)
+	startServer()
 
-	http.HandleFunc("/check_availability", availability.Check_availability(buses)) //pass date
-	http.HandleFunc("/book", book.BookingHandler(buses))                           // pass customer data, bus no, date
-	// http.HandleFunc("/print", PrintBooking)                   // pass booking id
-	// http.HandleFunc("/cancel", cancelBookingHandler)          // pass booking id
-
-	// Start server
-	// fmt.Println("Server is running...")
-	http.ListenAndServe(":8080", nil)
+	logger.Logs.Info().Msg("Main Function over")
 }
